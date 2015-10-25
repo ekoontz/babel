0ee73bc9fd863f62c5ea978b65c4a2c4deb9ae75
Eugene Koontz <ekoontz@hiro-tan.org>
2015-10-24 17:35:28 -0700

    fix typo

--- src/sql/cleanup_temp_tables.sh
+++ src/sql/cleanup_temp_tables.sh
@@ -4,5 +4,7 @@ if [ ! ${DATABASE_URL} ]; then
 fi
 
 echo "\d" |  psql -U ${USER} ${DB} | grep expression_import_ | awk '{print $3}' | xargs -I{} echo "drop table {};" | psql -U ${USER} ${DB}
+echo "\d" |  psql -U ${USER} ${DB} | grep expression_distinct_ | awk '{print $3}' | xargs -I{} echo "drop table {};" | psql -U ${USER} ${DB}
+
 
 
