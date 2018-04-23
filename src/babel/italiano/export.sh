9093baf010cf59ca0fdb94e61056292c5c34434c
Eugene Koontz <ekoontz@hiro-tan.org>
2018-04-23 09:02:00 -0700

    sort by target AND source ASC

--- src/babel/italiano/export.sh
+++ src/babel/italiano/export.sh
@@ -1,1 +1,1 @@
-echo "SELECT target,source,structure->>'vocabcoach-category' AS category FROM vocab_item WHERE active=true AND target_language='it' ORDER BY target ASC" | psql > vocab.txt
+echo "SELECT target,source,structure->>'vocabcoach-category' AS category FROM vocab_item WHERE active=true AND target_language='it' ORDER BY target ASC, source ASC" | psql > vocab.txt
