c0961a8de2dca10cc0505d6990da12592381cf32
Eugene Koontz <ekoontz@hiro-tan.org>
2024-12-12 21:46:34 +0100

    release 3.0.22-verbcoach

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject babel "3.0.20-verbcoach"
+(defproject babel "3.0.22-verbcoach"
   :description "A library for natural language generation and parsing"
   :url "http://github.com/ekoontz/babel"
   :license {:name "Eclipse Public License"
